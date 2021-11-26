4b06173db7b2589d8f9225c24c42a94cf6768a4d
Uzhastin-Nikita <fosfor20@mail.ru>
2021-11-26 15:44:34 +0300

    #2: update python.ts: addition new trigger

--- src/triggers/python.ts
+++ src/triggers/python.ts
@@ -1,13 +1,12 @@
 import { readdirSync } from 'fs';
 
 const TRIGGERS: Record<string, boolean> = {
-    '__init__': true,
-    'pipenv': true,
-    'Pipfile': true,
+    'requirements.txt': true,
 };
 
 const triggerPythonProject = (rootPath: string): Trigger | null => {
     if (readdirSync(rootPath).some((fileName: string) => TRIGGERS[fileName])) return Trigger.Python;
+    if (readdirSync(rootPath).some((fileName: string) => fileName.endsWith('.py'))) return Trigger.Python;
     return null;
 }
 

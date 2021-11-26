468e3cd5e40485a4523af70e6f1ad9fcc2e44f2b
Uzhastin-Nikita <fosfor20@mail.ru>
2021-11-26 15:52:42 +0300

    Rebased

--- src/triggers/python.ts
+++ src/triggers/python.ts
@@ -4,9 +4,9 @@ const TRIGGERS: Record<string, boolean> = {
     'requirements.txt': true,
 };
 
-const triggerPythonProject = (rootPath: string): Trigger | null => {
-    if (readdirSync(rootPath).some((fileName: string) => TRIGGERS[fileName])) return Trigger.Python;
-    if (readdirSync(rootPath).some((fileName: string) => fileName.endsWith('.py'))) return Trigger.Python;
+const triggerPythonProject = (rootPath: string): ProjectStack | null => {
+    if (readdirSync(rootPath).some((fileName: string) => TRIGGERS[fileName])) return ProjectStack.Python;
+    if (readdirSync(rootPath).some((fileName: string) => fileName.endsWith('.py'))) return ProjectStack.Python;
     return null;
 }
 

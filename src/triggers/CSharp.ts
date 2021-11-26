dc210ed7cb3e9b7b1c1562681d01439775d0cebb
Uzhastin-Nikita <fosfor20@mail.ru>
2021-11-26 18:50:10 +0300

    #3: update CSharp.ts: addition new cycle

--- src/triggers/CSharp.ts
+++ src/triggers/CSharp.ts
@@ -5,12 +5,17 @@ const TRIGGERS: Record<string, boolean> = {
 
 };
 
+
 const triggerCSharpProject = (rootPath: string): ProjectStack | null => {
-    if (readdirSync(rootPath).some((fileName: string) => TRIGGERS[fileName])) return ProjectStack.CSharp;
-    if (readdirSync(rootPath).some((fileName: string) => fileName.endsWith('.cs'))) return ProjectStack.CSharp;
-    if (readdirSync(rootPath).some((fileName: string) => fileName.endsWith('.sln'))) return ProjectStack.CSharp;
-    if (readdirSync(rootPath).some((fileName: string) => fileName.endsWith('.csproj'))) return ProjectStack.CSharp;
+    for (const fileName of readdirSync(rootPath)) {
+        if (TRIGGERS[fileName]) return ProjectStack.CSharp;
+        if (fileName.endsWith('.cs')) return ProjectStack.CSharp;
+        if (fileName.endsWith('.sln')) return ProjectStack.CSharp;
+        if (fileName.endsWith('.csproj')) return ProjectStack.CSharp;
+    }
     return null;
 }
+
+
 
 export default triggerCSharpProject;
